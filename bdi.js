39c764515a4d49cd0e111b93881d73e510f94122
fpalomerosanz <fpalomerosanz@gmail.com>
2023-08-18 21:29:11 -0400

    DOM

--- bdi.js
+++ bdi.js
@@ -6,3 +6,52 @@ baseDeIdeas.append(imput)
 document.querySelector("imput")
 
 const selector_interactivo = document.querySelector("#selector_interactivo");
+
+
+let genero = prompt("Bienvenido al Reproductor Intercativo de la Tía Botas. Escribe tu género favorito: aventuras o risas o fantasias")
+
+if(genero == "aventuras"){
+    alert("wow se vienen las aventuras")
+}
+if(genero == "risas"){
+    alert("a reir con las mejores historias")
+}
+if(genero == "fantasias"){
+    alert("imagina las mas locas historias")
+}
+
+let personaje = prompt("Escribe tu personaje favorito: unicornio o astronauta o dinosaurio")
+
+if(personaje == "unicornio"){
+    alert("muy buena seleción a cabalgar!")
+}
+if(personaje == "dinosaurio"){
+    alert("seguro tienes tu favorito")
+}
+if(personaje == "astronauta"){
+    alert("a disfrutar del cosmos")
+}
+
+let lugar = prompt("Escribe tu lugar favorito: bosque o espacio o castillo")
+
+if(lugar == "espacio"){
+    alert("3, 2, 1, despegue!")
+}
+if(lugar == "bosque"){
+    alert("hadas o gnomos de seguro")
+}
+if(lugar == "castillo"){
+    alert("las princesas han cambiado")
+}
+
+let elemento = prompt("Por último elijamos el elemento para tus historias entre: varita o bicicleta o cohete")
+
+if(elemento == "varita"){
+    alert("la magia está por comenzar...")
+}
+if(elemento == "bicicleta"){
+    alert("a rodar las aventuras!!!")
+}
+if(elemento == "cohete"){
+    alert("Huston tenemos un cuento, repito tenemos un cuento")
+}
--- selector_interactivo.js
+++ selector_interactivo.js
@@ -644,53 +644,67 @@ const arrayDeCuentos = [
         console.log(id);
     });
 
-let genero = prompt("Bienvenido al Reproductor Intercativo de la Tía Botas. Escribe tu género favorito: aventuras o risas o fantasias")
+const cuentoParaEscuchar = [];
+const verCuentos = () => {
+        let lista = 'lista de cuentos: \n' 
+        arrayDeCuentos.forEach(el => {
+            mensaje = mensaje + '${el.id} \n'
+        });
+        const opcion = parseInt(prompt(mensaje));
+        const cuentoElegido = arrayDeCuentos.find(cuento => cuento.id === opcion)
+        cuentoParaEscuchar.push(cuentoElegido)
+        alert("Excelente decision")
+    };
 
-if(genero == "aventuras"){
-    alert("wow se vienen las aventuras")
-}
-if(genero == "risas"){
-    alert("a reir con las mejores historias")
-}
-if(genero == "fantasias"){
-    alert("imagina las mas locas historias")
+const verGenero = arrayDeCuentos.filter((genero) => {
+    console.log(genero); while (opcion != 5 ) {
+        switch (opcion) {
+            case 1:
+                verGenero(); 
+                break;
+            case 2:
+                verPersonaje(); 
+                break;
+            case 3:
+                verlugar(); 
+                break;
+            case 4:
+                verElemento(); 
+                break;
+            default:
+                alert("ingresa una opcion valida")
+                break;
+        }
+    }
+});
+const verPersonaje =
+const verlugar =
+const verElemento =
+
+const elegirCuentos = () => {
+    let opcion =parseInt(prompt("Elige \n 1-Tu genero  \n 2-Tu personaje \n 3-Tu lugar \n 4-Tu elemento" ))
+    while (opcion != 5 ) {
+        switch (opcion) {
+            case 1:
+                verGenero(); 
+                break;
+            case 2:
+                verPersonaje(); 
+                break;
+            case 3:
+                verlugar(); 
+                break;
+            case 4:
+                verElemento(); 
+                break;
+            default:
+                alert("ingresa una opcion valida")
+                break;
+        }
+    }
 }
 
-let personaje = prompt("Escribe tu personaje favorito: unicornio o astronauta o dinosaurio")
 
-if(personaje == "unicornio"){
-    alert("muy buena seleción a cabalgar!")
-}
-if(personaje == "dinosaurio"){
-    alert("seguro tienes tu favorito")
-}
-if(personaje == "astronauta"){
-    alert("a disfrutar del cosmos")
-}
-
-let lugar = prompt("Escribe tu lugar favorito: bosque o espacio o castillo")
-
-if(lugar == "espacio"){
-    alert("3, 2, 1, despegue!")
-}
-if(lugar == "bosque"){
-    alert("hadas o gnomos de seguro")
-}
-if(lugar == "castillo"){
-    alert("las princesas han cambiado")
-}
-
-let elemento = prompt("Por último elijamos el elemento para tus historias entre: varita o bicicleta o cohete")
-
-if(elemento == "varita"){
-    alert("la magia está por comenzar...")
-}
-if(elemento == "bicicleta"){
-    alert("a rodar las aventuras!!!")
-}
-if(elemento == "cohete"){
-    alert("Huston tenemos un cuento, repito tenemos un cuento")
-}
 
 
 
